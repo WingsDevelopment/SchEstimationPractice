d66ec65cd6cfdb3a185f446bc334b71e7da49b0b
WingsDevelopment <bans.cowboy@gmail.com>
2021-04-20 16:38:17 +0200

    Added design to webclient

--- Applications/Applications.WebClient/Startup.cs
+++ Applications/Applications.WebClient/Startup.cs
@@ -1,13 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Core.ApplicationServices;
+using Core.Domain.Repositories;
+using Core.Domain.Services.External;
+using Core.Domain.Services.Internal;
+using Core.Domain.Services.Internal.Interfaces;
+using Core.Infrastructure.DataAccess.EfCoreDataAccess;
+using Core.Infrastructure.Services.RakicRaiffeisenBrosBankService.Mock;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace Applications.WebClient
 {
@@ -23,6 +27,34 @@ namespace Applications.WebClient
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // DbContext, UnitOfWork and RequestValidation services
+            services.AddDbContextPool<CoreEfCoreDbContext>(options =>
+            {
+                string connString = Configuration["Query:ConnectionString"];
+                options.UseSqlServer(connString);
+            });
+
+            services.AddScoped<ICoreUnitOfWork, CoreEfCoreUnitOfWork>();
+            services.AddScoped<IBankService, RakicRaiffeisenBrosBankService>();
+
+            services.AddSingleton<IPassService, PassService>((serviceProvider) =>
+            {
+                string pass_min = Configuration["minPassConfig"];
+                string pass_max = Configuration["maxPassConfig"];
+
+                return new PassService(pass_min, pass_max);
+            });
+
+            services.AddScoped((IServiceProvider serviceProvider) =>
+            {
+                return new WalletService(
+                    serviceProvider.GetRequiredService<ICoreUnitOfWork>(),
+                    serviceProvider.GetRequiredService<IBankService>(),
+                    serviceProvider.GetRequiredService<IPassService>());
+            });
+
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
             services.AddRazorPages();
         }
 
@@ -49,7 +81,9 @@ namespace Applications.WebClient
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }

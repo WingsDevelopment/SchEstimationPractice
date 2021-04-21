94cd31ce738e02ede11ba7af601b24dda6e4c804
WingsDevelopment <bans.cowboy@gmail.com>
2021-04-21 18:40:36 +0200

    Implemented transaction dto

--- Core.ApplicationServices/DTOs/TransactionDTO.cs
+++ Core.ApplicationServices/DTOs/TransactionDTO.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using Core.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Core.ApplicationServices.DTOs
 {
-    class TransactionDTO
+    public class TransactionDTO
     {
+        public string Id { get; set; }
+        public string WalletId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public TransactionType TransactionType { get; set; }
+        public string RelatedWalletJmbg { get; set; }
+        public string ReferenceTransactionId { get; set; }
+        public TransactionDTO()
+        {
+        }
+
+        public TransactionDTO(Transaction transaction)
+        {
+            Id = transaction.Id;
+            WalletId = transaction.WalletId;
+            Amount = transaction.Amount;
+            TransactionDate = transaction.TransactionDate;
+            TransactionType = transaction.TransactionType;
+            RelatedWalletJmbg = transaction.RelatedWalletJmbg;
+            ReferenceTransactionId = transaction.ReferenceTransactionId;
+        }
     }
 }

ced413cebfafaceeb47c2ec91201761d7213641e
WingsDevelopment <bans.cowboy@gmail.com>
2021-04-21 18:40:26 +0200

    Added get list of transactions method in transaction service

--- Core.ApplicationServices/TransactionService.cs
+++ Core.ApplicationServices/TransactionService.cs
@@ -1,7 +1,9 @@
-﻿using Core.Domain.Repositories;
+﻿using Core.ApplicationServices.DTOs;
+using Core.Domain.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Core.ApplicationServices
 {
@@ -16,6 +18,17 @@ namespace Core.ApplicationServices
             UnitOfWork = unitOfWork;
         }
 
+        public async Task<IEnumerable<TransactionDTO>> GetTransactionsByWalletId(string walletId)
+        {
+            var transactions = await UnitOfWork.TransactionRepository.GetFilteredList(w => w.WalletId == walletId);
 
+            List<TransactionDTO> transactionDTOs = new List<TransactionDTO>();
+            foreach(var transaction in transactions)
+            {
+                transactionDTOs.Add(new TransactionDTO(transaction));
+            }
+
+            return transactionDTOs;
+        }
     }
 }
